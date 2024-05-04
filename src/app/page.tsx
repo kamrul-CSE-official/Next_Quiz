a24a92d3cc011958d415a448082eeca4cc172945
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-05-05 01:11:02 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Bismaiila
+      <section>
+        <Button>Button</Button>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
